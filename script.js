a290353cd70ddeaf58641774456776b7030c3b63
DominikKnapczyk <dominik.kna.01@gmail.com>
2023-02-07 15:15:19 +0100

    + BONUS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //  Milestone 0:
-
+// Creazione dell'array di membri del team
 const team = [
   {
     name: "Wayne Barnett",
@@ -33,6 +33,8 @@ const team = [
   }
 ];
 
+// Milestone 1
+// Stampa delle informazioni sulla console
 team.forEach(member => {
   console.log(`Name: ${member.name}`);
   console.log(`Role: ${member.role}`);
@@ -41,8 +43,10 @@ team.forEach(member => {
 
 // Milestone 2
 
+// Selezioniamo il contenitore sul DOM in cui inseriremo i membri del team
 const teamContainer = document.getElementById("team-container");
 
+// Utilizziamo ancora il metodo forEach() per iterare attraverso ogni membro del team
 team.forEach(member => {
   const memberDiv = document.createElement("div");
   memberDiv.innerHTML = `
@@ -50,5 +54,34 @@ team.forEach(member => {
     <p>Role: ${member.role}</p>
     <p>Photo: ${member.photo}</p>
   `;
+  teamContainer.appendChild(memberDiv);
+});
+
+// BONUS 1
+
+team.forEach(member => {
+   // Creazione della struttura per ogni membro del team
+  const memberDiv = document.createElement("div");
+  memberDiv.innerHTML = `
+    <p>Name: ${member.name}</p>
+    <p>Role: ${member.role}</p>
+    <img src="./img/${member.photo}" alt="${member.name}" />
+  `;
+  // Aggiunta della struttura al container principale
+  teamContainer.appendChild(memberDiv);
+});
+
+// BONUS 2
+
+team.forEach(member => {
+   // Creazione della struttura per ogni membro del team
+  const memberDiv = document.createElement("div");
+  memberDiv.classList.add("team-member");
+  memberDiv.innerHTML = `
+    <img src="./img/${member.photo}" alt="${member.name}" />
+    <h3>${member.name}</h3>
+    <p>${member.role}</p>
+  `;
+  // Aggiunta della struttura al container principale
   teamContainer.appendChild(memberDiv);
 });
